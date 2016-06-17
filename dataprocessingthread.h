5dcc3858b56f397d6ba63940a794b5d5f2380e56
kimphg <pkp013@gmail.com>
2016-06-17 08:36:37 +0700

    convert IPLimage

--- dataprocessingthread.h
+++ dataprocessingthread.h
@@ -13,10 +13,10 @@
 //#include <opencv2/core/core.hpp>
 //#include <opencv2/imgproc/imgproc.hpp>
 
-#include "opencv/cv.h"
-#include "opencv/highgui.h"
-#include "opencv2/opencv.hpp"
-#include "opencv2/core/core.hpp"
+//#include "opencv/cv.h"
+//#include "opencv/highgui.h"
+//#include "opencv2/opencv.hpp"
+//#include "opencv2/core/core.hpp"
 #define HAVE_REMOTE
 #include "pcap.h"
 #ifndef CONST_NM
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -32,6 +32,7 @@ static char                 gridOff = false;
 static char                 udpFailure = 0;//config file !!!
 static bool                 isScreenUp2Date,isSettingUp2Date,isDraging = false;
 static bool                 isScaleChanged =true;
+QImage *img;
 static QStandardItemModel   modelTargetList;
 enum drawModes{
     SGN_DIRECT_DRAW,SGN_IMG_DRAW,NOTERR_DRAW
@@ -929,7 +930,7 @@ void Mainwindow::paintEvent(QPaintEvent *event)
         p.setPen(QPen(Qt::black));
         p.setBrush(QBrush(Qt::black));
         p.drawRect(rect);
-        p.drawImage(rect,*processing->radarData->img_zoom_ppi,processing->radarData->img_zoom_ppi->rect());
+        p.drawImage(rect,*img,img->rect());
     }
 }
 //void MainWindow::keyPressEvent(QKeyEvent *event)
@@ -2128,11 +2129,215 @@ void Mainwindow::on_toolButton_centerView_2_clicked()
     ShowVideoCam();
 
 }
+QImage *IplImageToQImage(const IplImage * iplImage, uchar **data, double mini, double maxi)
+{
+    uchar *qImageBuffer = NULL;
+    int width = iplImage->width;
+    int widthStep = iplImage->widthStep;
+    int height = iplImage->height;
+    QImage *qImage;
+    switch (iplImage->depth)
+    {
+        case IPL_DEPTH_8U:
+        if (iplImage->nChannels == 1)
+        {
+        // OpenCV image is stored with one byte grey pixel. We convert it
+       // to an 8 bit depth QImage.
+        qImage = new QImage(width,height,QImage::Format_Indexed8);
+        uchar *QImagePtr = qImage->scanLine(0);
+        qImageBuffer = qImage->scanLine(0);
 
+        const uchar *iplImagePtr = (const uchar *) iplImage->imageData;
+
+        for (int y = 0; y < height; y++)
+        {
+            // Copy line by line
+            QImagePtr = qImage->scanLine(y);
+            memcpy(QImagePtr, iplImagePtr, width);
+            iplImagePtr += widthStep;
+
+        }
+        /*
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                // We take only the highest part of the 16 bit value. It is
+                //similar to dividing by 256.
+                //*QImagePtr++ = ((*iplImagePtr++) >> 8);
+                *QImagePtr = *iplImagePtr;
+                QImagePtr++;
+                iplImagePtr++;
+                }
+
+                iplImagePtr += widthStep/sizeof(uchar)-width;
+            }*/
+        }
+        else if (iplImage->nChannels == 3)
+            {
+            /* OpenCV image is stored with 3 byte color pixels (3 channels).
+            We convert it to a 32 bit depth QImage.
+            */
+            qImageBuffer = (uchar *) malloc(width*height*4*sizeof(uchar));
+            uchar *QImagePtr = qImageBuffer;
+            const uchar *iplImagePtr = (const uchar *) iplImage->imageData;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // We cannot help but copy manually.
+                    QImagePtr[0] = iplImagePtr[0];
+                    QImagePtr[1] = iplImagePtr[1];
+                    QImagePtr[2] = iplImagePtr[2];
+                    QImagePtr[3] = 0;
+
+                    QImagePtr += 4;
+                    iplImagePtr += 3;
+                }
+            iplImagePtr += widthStep-3*width;
+            }
+
+        }
+        else
+        {
+            qDebug("IplImageToQImage: image format is not supported : depth=8U and %d channels\n", iplImage->nChannels);
+        }
+        break;
+        case IPL_DEPTH_16U:
+        if (iplImage->nChannels == 1)
+        {
+        /* OpenCV image is stored with 2 bytes grey pixel. We convert it
+        to an 8 bit depth QImage.
+        */
+            qImage = new QImage(width,height,QImage::Format_Indexed8);
+            uchar *QImagePtr = qImage->scanLine(0);
+            qImageBuffer = qImage->scanLine(0);
+
+            //const uint16_t *iplImagePtr = (const uint16_t *);
+            const unsigned short *iplImagePtr = (const unsigned short *)iplImage->imageData;
+
+
+            for (int y = 0; y < height; y++)
+            {
+                QImagePtr = qImage->scanLine(y);
+                for (int x = 0; x < width; x++)
+                {
+                // We take only the highest part of the 16 bit value. It is
+                //similar to dividing by 256.
+                //*QImagePtr++ = ((*iplImagePtr++) >> 8);
+                //change here 16 bit could be everything !! set max min to your desire
+                *QImagePtr = 255*(((*iplImagePtr) - mini) / (maxi - mini));
+                QImagePtr++;
+                iplImagePtr++;
+                }
+
+                iplImagePtr += widthStep/sizeof(unsigned short)-width;
+            }
+
+        }
+        else
+        {
+            qDebug("IplImageToQImage: image format is not supported : depth=16U and %d channels\n", iplImage->nChannels);
+
+        }
+        break;
+        case IPL_DEPTH_32F:
+         if (iplImage->nChannels == 1)
+         {
+        /* OpenCV image is stored with float (4 bytes) grey pixel. We
+        convert it to an 8 bit depth QImage.
+        */
+             qImage = new QImage(width,height,QImage::Format_Indexed8);
+             uchar *QImagePtr = qImage->scanLine(0);
+             qImageBuffer = qImage->scanLine(0);
+
+             const float *iplImagePtr = (const float *) iplImage->imageData;
+             for (int y = 0; y < height; y++)
+             {
+             QImagePtr = qImage->scanLine(y);
+                 for (int x = 0; x < width; x++)
+                 {
+                     uchar p;
+                     float pf = 255 * ((*iplImagePtr++) - mini) / (maxi - mini);
+                     if (pf < 0) p = 0;
+                     else if (pf > 255) p = 255;
+                     else p = (uchar) pf;
+
+                     *QImagePtr++ = p;
+                  }
+
+             iplImagePtr += widthStep/sizeof(float)-width;
+             }
+         }
+         else
+         {
+             qDebug("IplImageToQImage: image format is not supported : depth=32F and %d channels\n", iplImage->nChannels);
+         }
+       break;
+       case IPL_DEPTH_64F:
+         if (iplImage->nChannels == 1)
+         {
+            /* OpenCV image is stored with double (8 bytes) grey pixel. We
+            convert it to an 8 bit depth QImage.
+            */
+             qImage = new QImage(width,height,QImage::Format_Indexed8);
+             uchar *QImagePtr = qImage->scanLine(0);
+             qImageBuffer = qImage->scanLine(0);
+
+            const double *iplImagePtr = (const double *) iplImage->imageData;
+            for (int y = 0; y < height; y++)
+            {
+             QImagePtr = qImage->scanLine(y);
+                for (int x = 0; x < width; x++)
+                {
+                    uchar p;
+                    double pf = 255 * ((*iplImagePtr++) - mini) / (maxi - mini);
+
+                    if (pf < 0) p = 0;
+                    else if (pf > 255) p = 255;
+                    else p = (uchar) pf;
+
+                    *QImagePtr++ = p;
+                }
+
+            }
+
+        }
+        else
+        {
+            qDebug("IplImageToQImage: image format is not supported : depth=64F and %d channels\n", iplImage->nChannels);
+        }
+        break;
+        default:
+        qDebug("IplImageToQImage: image format is not supported : depth=%d and %d channels\n", iplImage->depth, iplImage->nChannels);
+    }
+
+    QVector<QRgb> vcolorTable;
+    if (iplImage->nChannels == 1)
+    {
+        // We should check who is going to destroy this allocation.
+        vcolorTable.resize(256);
+        for (int i = 0; i < 256; i++)
+        {
+           vcolorTable[i] = qRgb(i, i, i);
+        }
+        //Qt vector is difficult to use... start with std to qvector
+        //here I allocate QImage using qt constructor (Forma_Indexed8 adds sometimes 2 bytes on the right side !!! o.O not specified nowhere !!!)
+        //qImage = new QImage(tmpImg->scanLine(0), width, height, QImage::Format_Indexed8);
+        qImage->setColorTable(vcolorTable);
+    }
+    else
+    {
+        qImage = new QImage(qImageBuffer, width, height, QImage::Format_RGB32);
+    }
+    //*data = qImageBuffer;
+    return qImage;
+}
 void Mainwindow::ShowVideoCam()
 {
     cvNamedWindow("Video", 0);
     g_TrueFrame = cvQueryFrame(g_Capture);
+
     if (!g_TrueFrame)
             return;
 
@@ -2141,8 +2346,9 @@ void Mainwindow::ShowVideoCam()
         g_TrueFrame = cvQueryFrame(g_Capture);
         if (!g_TrueFrame)
                     break;
-
-        cvShowImage("Video", g_TrueFrame);
+        img = IplImageToQImage(g_TrueFrame,NULL,0,255);
+        repaint();
+        //cvShowImage("Video", g_TrueFrame);
         if (cvWaitKey(1) == 0)
                     break;
     }
