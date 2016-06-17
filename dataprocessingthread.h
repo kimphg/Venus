ef10603bcd529b453da34702db8cc48598dc68c0
quangthi <thinqmta@gmail.com>
2016-06-17 07:54:39 +0700

    ddd
    ddsdd

--- dataprocessingthread.h
+++ dataprocessingthread.h
@@ -12,6 +12,11 @@
 //#include <opencv2/highgui/highgui.hpp>
 //#include <opencv2/core/core.hpp>
 //#include <opencv2/imgproc/imgproc.hpp>
+
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
 #define HAVE_REMOTE
 #include "pcap.h"
 #ifndef CONST_NM
